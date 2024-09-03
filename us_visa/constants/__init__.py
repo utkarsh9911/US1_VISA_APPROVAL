6471e44e786e9545014cd3597ab3d38eeae49c1f
utkarsh9911 <utkarshraj7217@gmail.com>
2024-09-03 22:13:31 +0530

    data validation compelted

--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -21,7 +21,8 @@ MODEL_FILE_NAME = "model.pkl"
 
 
 
-
+PREPROCSSING_OBJECT_FILE_NAME = "preprocessing.pkl"
+SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
 
 
 """
@@ -32,3 +33,12 @@ DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
+
+
+"""
+Data Validation realted contant start with DATA_VALIDATION VAR NAME
+"""
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
+
